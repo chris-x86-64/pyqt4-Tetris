2b276382d5a0d3c33b656c8cd880ab2a20970db6
Christopher Smith <chris@x86-64.info>
2013-09-11 03:14:33 +0900

    added start method to start the game

--- libs/Tetris/Board.py
+++ libs/Tetris/Board.py
@@ -1,4 +1,21 @@
+from PyQt4 import QtGui, QtCore
+
 class Board(QtGui.QFrame):
 	BoardWidth = 10  # This defines the width of the game board (10 blocks)
 	BoardHeight = 22 # This defines the height of the game board (22 blocks)
 	Speed = 300 # This defines the game's speed (300 milliseconds)
+
+	def __init__(self, parent):
+
+		"""
+		Initializes QFrame.
+		Sets timer.
+		"""
+
+		QtGui.QFrame.__init__(self, parent)
+
+		self.timer = QtCore.QBasicTimer()
+
+	def start(self):
+
+		self.timer.start(Board.Speed, self)
