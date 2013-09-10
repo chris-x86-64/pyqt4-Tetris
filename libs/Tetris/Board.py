a9642bc604c0681df46c5bd4b46bcbd482eeddfd
Christopher Smith <chris@x86-64.info>
2013-09-11 02:57:09 +0900

    basic attributes of the game board

---
+++ libs/Tetris/Board.py
@@ -0,0 +1,4 @@
+class Board(QtGui.QFrame):
+	BoardWidth = 10  # This defines the width of the game board (10 blocks)
+	BoardHeight = 22 # This defines the height of the game board (22 blocks)
+	Speed = 300 # This defines the game's speed (300 milliseconds)
